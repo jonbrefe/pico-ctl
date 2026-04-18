c1141f92bc63b7b3407f962d97d3b4bf51e3b279
Jonathan Brenes <jonathan.brenes@microsoft.com>
2026-04-17 23:12:08 -0600

    release: v0.2.0
    - Add pico_ctl.1 man page (all 22 subcommands)
    - Add GitHub Release install instructions to README

--- pico-ctl.spec
+++ pico-ctl.spec
@@ -1,5 +1,5 @@
 Name:           pico-ctl
-Version:        0.1.0
+Version:        0.2.0
 Release:        1%{?dist}
 Summary:        All-in-one CLI for managing a Raspberry Pi Pico over USB serial
 License:        MIT
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pico-ctl',
-    version='0.1.0',
+    version='0.2.0',
     description='All-in-one CLI for managing a Raspberry Pi Pico over USB serial',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
